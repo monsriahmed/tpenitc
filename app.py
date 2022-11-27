f42904d42719723d0651969507b64ee238f0a040
unknown <ahmed.monsri@etudiant-enit.utm.tn>
2022-11-27 12:24:07 +0100

    first commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 # This example uses Python 2.7 and the python-request library.
 
-from requests import Request, Session
+from requests import request,Session
 from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
 import json
+from dotenv import load_dotenv
+import os
 
 
+def configure():
+    load_dotenv()
+    
 def extract_price(response):
     data = json.loads(response.text)
     return data["data"][0]["quote"]["USD"]["price"]
@@ -15,7 +20,7 @@ URL = "https://pro-api.coinmarketcap.com/v2/tools/price-conversion"
 PARAMETERS = {"amount": 1, "symbol": CRYPTO}
 HEADERS = {
     "Accepts": "application/json",
-    "X-CMC_PRO_API_KEY": "0e36cdb5-6e3e-48cb-a2ab-cae701524d8a",
+    "X-CMC_PRO_API_KEY": os.getenv('api_key'),
 }
 
 session = Session()
